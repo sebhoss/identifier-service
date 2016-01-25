a1b8462e11260c499dcbdca91ea45f5731f4d986
Sebastian Hoß <mail@shoss.de>
2016-01-25 21:27:49 +0100

    clipboard fix
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- identifier-http-service/src/main/resources/static/js/clipboard.js
+++ identifier-http-service/src/main/resources/static/js/clipboard.js
@@ -1,47 +1,51 @@
 "use strict";
 
-function copyToClipboard(preElement) {
-    if (preElement) {
-        let range = document.createRange();
-        range.selectNode(preElement);
-        window.getSelection().addRange(range);
-
-        try {
-            document.execCommand('copy');
-        } catch(err) {
-            console.log('Oops, unable to copy');
-        }
-
-        window.getSelection().removeAllRanges();
-    }
-};
-
 const notice1 = '<div class="alert-box notice">';
 const notice2 = '<span class="alert-type">notice: </span>Clipboard now contains: ';
 const notice3 = '</div>';
 
+var copyToClipboard = (function() {
+    var _dataString = null;
+
+    document.addEventListener("copy", function(e) {
+        if (_dataString !== null) {
+            try {
+                e.clipboardData.setData("text/plain", _dataString);
+                e.preventDefault();
+            } finally {
+                _dataString = null;
+            }
+        }
+    });
+
+    return function(data) {
+        _dataString = data;
+        document.execCommand("copy");
+    };
+})();
+
 // DOMContentLoaded
 window.addEventListener('WebComponentsReady', function(e) {
-    let identifiers = document.querySelectorAll('pre');
+    let preElements = document.querySelectorAll('pre');
 
-    for (let index in identifiers) {
-      let id = identifiers[index];
-      if (id && id.textContent) {
-        id.addEventListener('click', function(event) {
-          let alert = document.querySelector('#alert-container');
-          let notice = notice1 + notice2 + '<span>' + id.textContent.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') ; + '</span>' + notice3;
-          let tmp = document.createElement('div');
-          tmp.innerHTML = notice;
+    for (let index in preElements) {
+        let preElement = preElements[index];
+        if (preElement && preElement.textContent) {
+            preElement.addEventListener('click', function(event) {
+                let value = preElement.textContent.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+                let alert = document.querySelector('#alert-container');
+                let notice = notice1 + notice2 + '<span>' + value + '</span>' + notice3;
+                let tmp = document.createElement('div');
+                tmp.innerHTML = notice;
 
-          alert.appendChild(tmp)
+                alert.appendChild(tmp)
+                copyToClipboard(preElement.textContent)
 
-          copyToClipboard(id);
-
-          setTimeout(function() {
-              alert.removeChild(tmp);
-          }, (1000));
-        });
-      }
+                setTimeout(function() {
+                    alert.removeChild(tmp);
+                }, (1000));
+            });
+        }
     }
 
 });
