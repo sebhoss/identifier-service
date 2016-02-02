89a74762e378a4dc4a3f24041453ad73387bf6ae
Sebastian Hoß <mail@shoss.de>
2016-02-02 20:44:09 +0100

    default to '1' in case the user removed the quantity value from the URL
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- identifier-http-service/src/main/java/com/github/sebhoss/identifier/usecases/IndexPages.java
+++ identifier-http-service/src/main/java/com/github/sebhoss/identifier/usecases/IndexPages.java
@@ -404,7 +404,7 @@ public class IndexPages {
     @SafeVarargs
     private static String index(final Model model, final Integer quantity, final Consumer<Model>... consumers) {
         Arrays.stream(consumers).forEach(consumer -> consumer.accept(model));
-        model.addAttribute("quantity", quantity);
+        model.addAttribute("quantity", quantity == null ? Integer.valueOf(1) : quantity);
         return "index"; // delegates to the 'index' template
     }
 
