3166856d4a85e4284fb7ab827fc51643245b4820
Sebastian Hoß <mail@shoss.de>
2017-10-08 22:52:13 +0200

    apply code style

--- identifier-http-service/src/test/java/com/github/sebhoss/identifier/testsupport/AbstractMockMvcTest.java
+++ identifier-http-service/src/test/java/com/github/sebhoss/identifier/testsupport/AbstractMockMvcTest.java
@@ -31,59 +31,59 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
  * Abstract unit test that sets up Spring's MockMvc.
  *
  * @param <CONTROLLER>
- *          The type of the controller to test.
+ *            The type of the controller to test.
  * @param <API>
- *          The type of the controller's API to use.
+ *            The type of the controller's API to use.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public abstract class AbstractMockMvcTest<CONTROLLER, API> {
 
-  protected String                        expectedResult;
-  protected Supplier<String>              supplier;
-  protected API                           api;
+    protected String                        expectedResult;
+    protected Supplier<String>              supplier;
+    protected API                           api;
 
-  private final Function<API, CONTROLLER> constructor;
-  private final Class<API>                apiClass;
+    private final Function<API, CONTROLLER> constructor;
+    private final Class<API>                apiClass;
 
-  private MockMvc                         mvc;
+    private MockMvc                         mvc;
 
-  protected AbstractMockMvcTest(
-      final Function<API, CONTROLLER> constructor,
-      final Class<API> apiClass) {
-    this.constructor = constructor;
-    this.apiClass = apiClass;
-  }
+    protected AbstractMockMvcTest(
+            final Function<API, CONTROLLER> constructor,
+            final Class<API> apiClass) {
+        this.constructor = constructor;
+        this.apiClass = apiClass;
+    }
 
-  /**
-   * Sets up both API and MVC.
-   *
-   * @throws Exception
-   *           In case something goes wrong.
-   */
-  @Before
-  public final void setUp() throws Exception {
-    api = Mockito.mock(apiClass);
-    mvc = MockMvcBuilders.standaloneSetup(constructor.apply(api)).build();
-  }
+    /**
+     * Sets up both API and MVC.
+     *
+     * @throws Exception
+     *             In case something goes wrong.
+     */
+    @Before
+    public final void setUp() throws Exception {
+        api = Mockito.mock(apiClass);
+        mvc = MockMvcBuilders.standaloneSetup(constructor.apply(api)).build();
+    }
 
-  protected final void requestAndVerify(final String path) throws Exception {
-    given(supplier.get()).willReturn(expectedResult);
-    text(path).andExpect(status().isOk())
-        .andExpect(content().string(equalTo(expectedResult)));
-  }
+    protected final void requestAndVerify(final String path) throws Exception {
+        given(supplier.get()).willReturn(expectedResult);
+        text(path).andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expectedResult)));
+    }
 
-  protected final ResultActions text(final String path) throws Exception {
-    return mvc.perform(MockMvcRequestBuilders.get(path).accept(MediaType.TEXT_PLAIN));
-  }
+    protected final ResultActions text(final String path) throws Exception {
+        return mvc.perform(MockMvcRequestBuilders.get(path).accept(MediaType.TEXT_PLAIN));
+    }
 
-  protected final void verifyHtml(final String path) throws Exception {
-    html(path).andExpect(status().isOk());
-  }
+    protected final void verifyHtml(final String path) throws Exception {
+        html(path).andExpect(status().isOk());
+    }
 
-  protected final ResultActions html(final String path) throws Exception {
-    return mvc.perform(MockMvcRequestBuilders.get(path).accept(MediaType.TEXT_HTML));
-  }
+    protected final ResultActions html(final String path) throws Exception {
+        return mvc.perform(MockMvcRequestBuilders.get(path).accept(MediaType.TEXT_HTML));
+    }
 
 }
